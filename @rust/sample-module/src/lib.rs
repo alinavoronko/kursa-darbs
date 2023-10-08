a0e17e4f14456b3882c6b9145ef579988e3efb5c
Lasagna <lasagna.tss@gmail.com>
2023-10-08 16:55:25 +0300

    WIP1

--- @rust/sample-module/src/lib.rs
+++ @rust/sample-module/src/lib.rs
@@ -28,3 +28,26 @@ mod tests {
         assert_eq!(add(2, 2), 4)
     }
 }
+
+
+#[no_mangle]
+pub extern "C" fn perform_matrix_multiplication(
+    matrix1: Vec<Vec<i32>>,
+    matrix2: Vec<Vec<i32>>,
+) -> Vec<Vec<i32>> {
+    let m = matrix1.len();
+    let p = matrix2.len();
+    let n = matrix2[0].len();
+
+    let mut result: Vec<Vec<i32>> = vec![vec![0; n]; m];
+
+    for i in 0..m {
+        for j in 0..n {
+            for k in 0..p {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    result
+}
---
+++ func.js
@@ -0,0 +1,69 @@
+import { performMatrixMultiplication } from './martix_multiplication.js';
+
+export function multiplyMatrices() {
+    // Get the input matrices as strings from the text areas
+    const matrix1Str = document.getElementById("matrix1").value;
+    const matrix2Str = document.getElementById("matrix2").value;
+
+    // Parse the input matrices
+    const matrix1 = parseMatrix(matrix1Str);
+    const matrix2 = parseMatrix(matrix2Str);
+
+    // Check if the matrices are valid for multiplication
+    if (!isValidForMultiplication(matrix1, matrix2)) {
+      alert(
+        "Invalid matrices for multiplication. Matrix1 columns must be equal to Matrix2 rows."
+      );
+      return;
+    }
+
+    console.time("Matrix Multiplication");
+    // Perform matrix multiplication
+    const resultMatrix = performMatrixMultiplication(matrix1, matrix2);
+    console.timeEnd("Matrix Multiplication");
+
+    // Display the result
+    displayMatrix(resultMatrix, "result");
+  }
+
+  function parseMatrix(matrixStr) {
+    const rows = matrixStr.split("\n");
+    const matrix = rows.map((row) => row.split(",").map(Number));
+    return matrix;
+  }
+
+  function isValidForMultiplication(matrix1, matrix2) {
+    return matrix1[0].length === matrix2.length;
+  }
+
+//   function performMatrixMultiplication(matrix1, matrix2) {
+//     console.time("Matrix Multiplication");
+//     const result = [];
+//     const m = matrix1.length;
+//     const n = matrix2[0].length;
+//     const p = matrix2.length;
+
+//     for (let i = 0; i < m; i++) {
+//       result.push([]);
+//       for (let j = 0; j < n; j++) {
+//         result[i][j] = 0;
+//         for (let k = 0; k < p; k++) {
+//           result[i][j] += matrix1[i][k] * matrix2[k][j];
+//         }
+//       }
+//     }
+//     console.timeEnd("Matrix Multiplication");
+//     return result;
+//   }
+
+  function displayMatrix(matrix, elementId) {
+    const resultElement = document.getElementById(elementId);
+    resultElement.innerHTML = "";
+
+    for (let i = 0; i < matrix.length; i++) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        resultElement.innerHTML += matrix[i][j] + " ";
+      }
+      resultElement.innerHTML += "<br>";
+    }
+  }
